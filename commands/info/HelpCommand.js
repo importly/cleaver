61d9ff38ca5d9cc6a03d0baec4420b17dddc501a
importly <aryankuthakur@hotmail.com>
2021-01-29 15:50:58 -0500

    save

--- commands/info/HelpCommand.js
+++ commands/info/HelpCommand.js
@@ -1,17 +1,21 @@
 const { MessageEmbed, Message } = require("discord.js");
 const Client = require("../../client/Client");
 const BaseCommand = require("../../client/BaseCommand");
+const { } = require('../.')
 
 class HelpCommand extends BaseCommand {
   constructor() {
     super("ping", "Measures latency", "misc", ["latency"]);
   }
-  /**
+    /**
    *
    * @param {Client} client
    * @param {Message} message
    * @param {String[]} args
    */
-  async execute(client, message, args) {}
+
+  async execute(client, message, args) {
+    //ok so go to Client.js
+  }
 }
 module.exports = HelpCommand;
